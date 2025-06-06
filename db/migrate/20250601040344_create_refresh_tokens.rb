4729fb9d67ac1d82ebb03ce323674300c9285412
Daesun Pak <daesun.samuel0701993@outlook.com>
2025-06-05 23:33:05 -0400

    Fixed migration ordering issue by combining refresh token migrations.

--- db/migrate/20250601040344_create_refresh_tokens.rb
+++ db/migrate/20250601040344_create_refresh_tokens.rb
@@ -4,10 +4,22 @@ class CreateRefreshTokens < ActiveRecord::Migration[8.0]
       t.references :user, null: false, foreign_key: true
       t.string :token, null: false
       t.datetime :expires_at, null: false
+      t.integer :status, default: 0, null: false
+      t.datetime :revoked_at
+      t.text :revocation_reason
+      t.inet :revoked_from_ip
+      t.datetime :replaced_at
+      t.integer :replaced_by
+      t.inet :issued_from_ip
+      t.text :user_agent
 
       t.timestamps
     end
     
     add_index :refresh_tokens, :token, unique: true
+    add_index :refresh_tokens, :status
+    add_index :refresh_tokens, [:user_id, :status]
+    add_index :refresh_tokens, :revoked_at
+    add_index :refresh_tokens, :replaced_by
   end
 end
