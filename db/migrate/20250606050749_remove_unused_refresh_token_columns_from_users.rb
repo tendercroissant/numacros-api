f9a58b93db1c8b3ac4c3fef608dbe98500341014
Daesun Pak <daesun.samuel0701993@outlook.com>
2025-06-06 01:46:26 -0400

    Fixed

--- db/migrate/20250606050749_remove_unused_refresh_token_columns_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUnusedRefreshTokenColumnsFromUsers < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :users, :refresh_token_digest, :string
-    remove_column :users, :refresh_token_expires_at, :datetime
-  end
-end
